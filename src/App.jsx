158301fdc920698850cc2daa65b056d306971117
Joseph Gray <jgray3797@gmail.com>
2025-02-10 20:05:03 -0600

    eighth attempt

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { Routes, Route } from 'react-router-dom';
-import "../styles/style.css";
 import { CartProvider } from './context/cartContext';
 import Home from './pages/Home';
 import Movies from './pages/Movies';
